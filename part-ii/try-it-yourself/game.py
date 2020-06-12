64fa8377759e50002c55463de2540df9ac627006
kiskis <fakekis@yahoo.com>
2020-06-12 18:29:28 +0200

    update pcc

--- part-ii/try-it-yourself/game.py
+++ part-ii/try-it-yourself/game.py
@@ -8,31 +8,31 @@ from alien_boss import AlienBoss
 class Game:
     """Overall class to manage game assets and behaviour."""
 
-def __init__(self):
-    """Initialize the game, and create game resources."""
-    pygame.init()
-    self.settings = Settings()
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.settings = Settings()
 
-    self.screen = pygame.display.set_mode(
-        (self.settings.screen_width, self.settings.screen_height))
-    pygame.display.set_caption("My Game")
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("My Game")
 
-    self.alien_boss= AlienBoss(self)
+        self.alien_boss= AlienBoss(self)
 
-def run_game(self):
-    """Start the main loop for the game."""
-    while True:
-        #Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        #redraw the screen during each pass through the loop. 
-        self.screen.fill(self.bg_color)
-        self.alien_boss.blitme()
-        #make the most recently drawn screen visible.
-        pygame.display.flip()
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            #Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            #redraw the screen during each pass through the loop. 
+            self.screen.fill(self.settings.bg_color)
+            self.alien_boss.blitme()
+            #make the most recently drawn screen visible.
+            pygame.display.flip()
 
-if__name__ = '__main__':
+if __name__ == '__main__':
     #make a game instance, and run the game.
     ai = Game()
     ai.run_game()
