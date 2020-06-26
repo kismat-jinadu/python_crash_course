af2e6b79c1066be12cb4f5988edfff7d5892dc24
kiskis <fakekis@yahoo.com>
2020-06-26 16:52:55 +0200

    multiple random walks

--- part-ii/data_visualisation/rw_visual.py
+++ part-ii/data_visualisation/rw_visual.py
@@ -2,12 +2,19 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk 
 
-#make a random walk
-rw= RandomWalk()
-rw.fill_walk()
+#keep making new walks, as long as the program is active.
 
-#plot the points in the walk.
-plt.style.use('classic')
-fig,ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+while True:
+    #make a random walk
+    rw= RandomWalk()
+    rw.fill_walk()
+
+    #plot the points in the walk.
+    plt.style.use('classic')
+    fig,ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
