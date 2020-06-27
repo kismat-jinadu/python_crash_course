e5976379cc16312360043bfcb15a47295b0ab626
kiskis <fakekis@yahoo.com>
2020-06-27 17:36:45 +0200

    two data sets

--- part-ii/data_visualisation/sitka_highs.py
+++ part-ii/data_visualisation/sitka_highs.py
@@ -7,21 +7,24 @@ filename='data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    #get dates and high temperatures from this file.
-    dates,highs =[], []
+    #get dates, high and low temperatures from this file.
+    dates,highs, lows =[], [], []
     for row in reader:
         current_date = datetime.strptime(row[2],'%Y-%m-%d')
         high =int(row[5])
+        low =int(row[6])
         dates.append(current_date)
         highs.append(high)
+        lows.append(low)
 
-#plot the high temperatures.
+#plot the high and low temperatures.
 plt.style.use('seaborn')
 fig,ax =plt.subplots()
 ax.plot(dates,highs, c='red')
+ax.plot(dates,lows, c='blue')
 
 #format plot.
-ax.set_title("Daily high temperatures - 2018",fontsize =24)
+ax.set_title("Daily high and low temperatures - 2018",fontsize =24)
 ax.set_xlabel('',fontsize =16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)",fontsize = 16)
