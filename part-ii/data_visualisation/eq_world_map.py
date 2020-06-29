f51054706c9f6b868e93e7419b1de5df3b5b51ea
kiskis <fakekis@yahoo.com>
2020-06-29 19:34:08 +0200

    customized markers on 30day data

--- part-ii/data_visualisation/eq_world_map.py
+++ part-ii/data_visualisation/eq_world_map.py
@@ -4,7 +4,7 @@ from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
 #explore the structure of the data.
-filename = 'data/eq_data_1_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
@@ -26,6 +26,10 @@ data = [{
     'lat':lats,
     'marker':{
         'size':[5*mag for mag in mags],
+        'color':mags,
+        'colorscale':'Viridis',
+        'reversescale':True,
+        'colorbar':{'title':'Magnitude'},
     },
 }]
 my_layout = Layout(title = 'Global Earthquakes')
