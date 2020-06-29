00e3d31c7f965b3d99c59efc6cc9404c2f229135
kiskis <fakekis@yahoo.com>
2020-06-29 19:25:58 +0200

    earthquake map with initial markers

--- part-ii/data_visualisation/eq_world_map.py
+++ part-ii/data_visualisation/eq_world_map.py
@@ -20,7 +20,14 @@ for eq_dict in all_eq_dicts:
     lats.append(lat)
 
 #Map the earthquakes.
-data = [Scattergeo(lon=lons, lat=lats)]
+data = [{
+    'type':'scattergeo',
+    'lon':lons,
+    'lat':lats,
+    'marker':{
+        'size':[5*mag for mag in mags],
+    },
+}]
 my_layout = Layout(title = 'Global Earthquakes')
 
 fig = {'data':data, 'layout':my_layout}
