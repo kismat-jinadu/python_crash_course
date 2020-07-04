b5c1b025beae19588d9c6bc7c25f365ee613269b
kiskis <fakekis@yahoo.com>
2020-07-04 13:12:45 +0200

    working with response dict

--- part-ii/data_visualisation/python_repos.py
+++ part-ii/data_visualisation/python_repos.py
@@ -7,6 +7,14 @@ r = requests.get(url,headers = headers)
 print(f"Status code:{r.status_code}")
 #store API response in a variable.
 response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
 
-#process results.
-print(response_dict.keys())
+#Explore information about the respositories.
+repo_dicts = response_dict['items']
+print(f"Repositories returned:{len(repo_dicts)}")
+
+#examine the first repository.
+repo_dict =repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
