43c61f493e754056da2b7ac42fe14221ba5dc96f
kiskis <fakekis@yahoo.com>
2020-07-04 15:18:48 +0200

    custom tooltips added

--- part-ii/data_visualisation/python_repos_visual.py
+++ part-ii/data_visualisation/python_repos_visual.py
@@ -12,21 +12,41 @@ print(f"Status code:{r.status_code}")
 #process results.
 response_dict = r.json()
 repo_dicts = response_dict['items']
-repo_names,stars =[],[]
+repo_names,stars,labels =[],[],[]
 for repo_dict in repo_dicts:
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
+
+    owner = repo_dict['owner']['login']
+    description=repo_dict['description']
+    label=f"{owner}<br/>{description}"
+    labels.append(label)
 
 #Make visualisation.
 data =[{
     'type':'bar',
     'x':repo_names,
-    'y':stars,   
+    'y':stars,
+    'hovertext':labels,
+    'marker':{
+        'color':'rgb(60,100,150)',
+        'line':{'width':1.5,'color':'rgb(25,25,25)'}
+    },
+    'opacity':0.6,   
 }]
 my_layout = {
     'title':'Most-Starred Pyhton Projects on GitHub',
-    'xaxis':{'title':'Repository'},
-    'yaxis':{'title':'Stars'},
+    'titlefont':{'size':28},
+    'xaxis':{
+        'title':'Repository',
+        'titlefont':{'size':24},
+        'tickfont':{'size':14},
+    },
+    'yaxis':{
+        'title':'Stars',
+        'titlefont': {'size':24},
+        'tickfont': {'size':14},
+    },
 }
 
 fig = {'data':data, 'layout':my_layout}
