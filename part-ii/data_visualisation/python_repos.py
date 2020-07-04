bb827052b4150a4323fcdd04679abdb0ddfa8b3a
kiskis <fakekis@yahoo.com>
2020-07-04 13:49:42 +0200

    top repos

--- part-ii/data_visualisation/python_repos.py
+++ part-ii/data_visualisation/python_repos.py
@@ -13,8 +13,12 @@ print(f"Total repositories: {response_dict['total_count']}")
 repo_dicts = response_dict['items']
 print(f"Repositories returned:{len(repo_dicts)}")
 
-#examine the first repository.
-repo_dict =repo_dicts[0]
-print(f"\nKeys: {len(repo_dict)}")
-for key in sorted(repo_dict.keys()):
-    print(key)
+
+print(f"\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print(f"Name:{repo_dict['name']}")
+    print(f"Owner:{repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Created: {repo_dict['html_url']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
