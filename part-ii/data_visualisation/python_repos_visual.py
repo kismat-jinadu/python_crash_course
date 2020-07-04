c0a5d88bed9db9ddd505e2261021de3aec08128b
kiskis <fakekis@yahoo.com>
2020-07-04 14:43:44 +0200

    initial commit

---
+++ part-ii/data_visualisation/python_repos_visual.py
@@ -0,0 +1,33 @@
+import requests
+
+from plotly.graph_objs import Bar 
+from plotly import offline
+
+#Make an API call and store the response.
+url ='https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept':'application/vnd.github.v3+json'}
+r = requests.get(url,headers = headers)
+print(f"Status code:{r.status_code}")
+
+#process results.
+response_dict = r.json()
+repo_dicts = response_dict['items']
+repo_names,stars =[],[]
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+#Make visualisation.
+data =[{
+    'type':'bar',
+    'x':repo_names,
+    'y':stars,   
+}]
+my_layout = {
+    'title':'Most-Starred Pyhton Projects on GitHub',
+    'xaxis':{'title':'Repository'},
+    'yaxis':{'title':'Stars'},
+}
+
+fig = {'data':data, 'layout':my_layout}
+offline.plot(fig,filename='python_repos.html')
