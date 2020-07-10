09720d026de6fc19cafef0cba30aee7153b10e77
kiskis <fakekis@yahoo.com>
2020-07-10 18:24:22 +0200

    initial commit

---
+++ part-ii/learning_log/ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kismat/Documents/python-crash-course/part-ii/learning_log/ll_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ part-ii/try-it-yourself/web_app/snap_gram/snap_gram/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for snap_gram project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'snap_gram.settings')
+
+application = get_asgi_application()
