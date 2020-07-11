b3e9d7c1940e5ab5a393f448e0632f1294891d0c
kiskis <fakekis@yahoo.com>
2020-07-11 20:12:01 +0200

    individual pages added

--- part-ii/learning_log/learning_logs/urls.py
+++ part-ii/learning_log/learning_logs/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('',views.index,name='index'),
     #page that shows all topics.
     path('topics/',views.topics, name ='topics'),
+    #detail page for a single topic.
+    path('topics/<int:topic_id>/',views.topic, name='topic'),
 ]
