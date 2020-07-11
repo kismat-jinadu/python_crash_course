b8919869cce490af019c67ce030aaf95a400fa7e
kiskis <fakekis@yahoo.com>
2020-07-11 17:58:33 +0200

    additional pages built

--- part-ii/learning_log/learning_logs/urls.py
+++ part-ii/learning_log/learning_logs/urls.py
@@ -8,4 +8,6 @@ app_name = 'learning_logs'
 urlpatterns = [
     #Home page
     path('',views.index,name='index'),
+    #page that shows all topics.
+    path('topics/',views.topics, name ='topics'),
 ]
