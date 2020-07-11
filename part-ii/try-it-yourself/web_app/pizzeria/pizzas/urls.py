e20f090685d965024bc9a4ee51ed80e6bc915cc9
kiskis <fakekis@yahoo.com>
2020-07-11 21:01:14 +0200

    pages added(18-8)

--- part-ii/try-it-yourself/web_app/pizzeria/pizzas/urls.py
+++ part-ii/try-it-yourself/web_app/pizzeria/pizzas/urls.py
@@ -8,4 +8,8 @@ app_name = 'pizzas'
 urlpatterns = [
     #Home page
     path('',views.index,name='index'),
+    #page that shows all pizza names
+    path('pizza_names/',views.pizza_names, name ='pizza_names'),
+    #detail page for a single pizza_name.
+    path('pizza_names/<int:pizza_name_id>/',views.pizza_name, name='pizza_name'),
 ]
--- part-ii/try-it-yourself/web_app/pizzeria/pizzas/views.py
+++ part-ii/try-it-yourself/web_app/pizzeria/pizzas/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
+from .models import Pizza
 # Create your views here.
 
 def index(request):
     """The home page for Pizzas."""
     return render(request, 'pizzas/index.html')
+
+def pizza_names(request):
+    """Show all pizza_names."""
+    pizza_names = Pizza.objects.order_by('date_added')
+    context={'pizza_names':pizza_names}
+    return render(request, 'pizzas/pizza_names.html',context)
+
+def pizza_name(request, pizza_name_id):
+    """Show a single pizza_name and all its toppings."""
+    pizza_name = Pizza.objects.get(id=pizza_name_id)
+    toppings = pizza_name.topping_set.order_by('-date_added')
+    context ={'pizza_name':pizza_name, 'toppings':toppings}
+    return render(request,'pizzas/pizza_name.html',context)
+
