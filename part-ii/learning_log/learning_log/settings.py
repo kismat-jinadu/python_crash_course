55bef5d292f14ed3bf8c64ef58af0c64353555e3
kiskis <fakekis@yahoo.com>
2020-07-14 20:32:06 +0200

    styled home and login pages

--- part-ii/learning_log/learning_log/settings.py
+++ part-ii/learning_log/learning_log/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'learning_logs',
     'users',
 
+    #third party apps.
+    'bootstrap4',
+
     #Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
